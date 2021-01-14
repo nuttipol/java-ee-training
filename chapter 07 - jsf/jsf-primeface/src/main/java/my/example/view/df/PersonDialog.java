25e46d82747aab4ad99aa15b048f079b3a7f9217
Nuttipol Chalongmaneerat <nuttipolc@gmail.com>
2021-01-14 15:57:20 +0700

    pass parameter to dialog example

--- chapter 07 - jsf/jsf-primeface/src/main/java/my/example/view/df/PersonDialog.java
+++ chapter 07 - jsf/jsf-primeface/src/main/java/my/example/view/df/PersonDialog.java
@@ -1,37 +1,55 @@
 package my.example.view.df;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
 
 import org.primefaces.PrimeFaces;
 
 import lombok.Data;
 import my.example.data.Person;
 
-@ViewScoped
+@RequestScoped
 @ManagedBean
-@Data
+@Data 
 public class PersonDialog {
 
 	private Person person;
-
+	
     @PostConstruct
     public void init() {
+    	String data = FacesContext.getCurrentInstance()
+                .getExternalContext()
+                .getRequestParameterMap()
+                .get("data1");
+    	
     	person = new Person();
+    	person.setFirstName(data);
+    	
     }
      
 	public void choosePerson() {
-		Map<String, Object> options = new HashMap<>();
+		FacesContext fc = FacesContext.getCurrentInstance();
+        Map<String,String> parameter = 
+           fc.getExternalContext().getRequestParameterMap();
+        
+		Map<	String, Object> options = new HashMap<>();
 		options.put("resizable", false);
 		options.put("draggable", false);
 		options.put("width", 640);
 		options.put("height", 400);
 		options.put("modal", true);
-		PrimeFaces.current().dialog().openDynamic("add-person", options, null);
+		 
+		Map<String, List<String>> params = new HashMap<>();
+	    params.put("data1", Arrays.asList(parameter.get("param1")));
+	      
+		PrimeFaces.current().dialog().openDynamic("add-person", options, params);
 	}
 	
 
